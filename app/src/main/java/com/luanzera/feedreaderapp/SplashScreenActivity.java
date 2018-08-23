10e0c0eb06eb6a51da640497980b52dcddaf15d4
luanzera1 <luanz@live.com>
2018-08-22 22:09:36 -0300

    Consertado a tela de SplashScreen quando voltava da MainActivity.
    Att: 22/08/18

--- app/src/main/java/com/luanzera/feedreaderapp/SplashScreenActivity.java
+++ app/src/main/java/com/luanzera/feedreaderapp/SplashScreenActivity.java
@@ -1,22 +1,32 @@
 package com.luanzera.feedreaderapp;
 
-import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
-            }
-        }, 2000);
+
+        EasySplashScreen splashScreen = new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(5000)
+                .withBackgroundColor(Color.parseColor("#1087ff"))
+                .withLogo(R.drawable.icon)
+                .withBeforeLogoText("Tecmundo Reader")
+                .withFooterText("Desenvolvido por luanzera");
+
+        splashScreen.getBeforeLogoTextView().setTextColor(Color.WHITE);
+        splashScreen.getFooterTextView().setTextColor(Color.WHITE);
+
+        View view = splashScreen.create();
+        setContentView(view);
+
     }
 }
